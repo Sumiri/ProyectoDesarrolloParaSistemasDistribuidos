880c61e5ee5066fdecd0536b7620efd9d2121a61
Victor Jimenez <vjimenez@americatv.com.pe>
2015-10-07 12:37:37 -0500

    conexiones

--- Final/RESTServices/Persistencia/ConexionUtil.cs
+++ Final/RESTServices/Persistencia/ConexionUtil.cs
@@ -11,7 +11,13 @@ namespace RESTServices.Persistencia
         {
             get
             {
-                return "Data Source=TOSHIBA\\SQLEXPRESS;Initial Catalog=BD_PEDIDOS;Integrated Security=True";
+                //return "Data Source=CASA\\SQLEXPRESS;Initial Catalog=BD_Pedidos;Integrated Security=True";
+                //return "Data Source=.;Initial Catalog=BD_Pedidos;Integrated Security=SSPI;";
+                //return "Data Source=DESARROLLO_PC;Initial Catalog=BD_Pedidos;Integrated Security=SSPI;";
+                //return "Server=DESARROLLO_PC\\SQL2008R2;Database=BD_Pedidos;User Id=raponte; Password=xzfqxt";
+                //return "Server=JAIME-PC\\SQL2008R2;Database=BD_Pedidos;User Id=raponte; Password=xzfqxt";
+                //return "Data Source=TOSHIBA\\SQLEXPRESS;Initial Catalog=BD_Pedidos;Integrated Security=True";
+                return "Data Source=WSVIJIMENEZ\\SQLEXPRESS;Database=BD_Pedidos;User Id=vjimenez; Password=vji2002";
             }
         }
     }
